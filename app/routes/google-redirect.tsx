cc88e22c8fab88fc9dff395308b269430b67bd6a
fuaddm <dmirciyevfuad@gmail.com>
2025-10-12 23:34:00 +0400

    fix: try to fix google auth

--- app/routes/google-redirect.tsx
+++ app/routes/google-redirect.tsx
@@ -3,16 +3,15 @@ import { redirect } from 'react-router';
 
 export async function loader({ request }: Route.ClientActionArgs) {
   const url = new URL(request.url);
-  const accessToken = url.searchParams.get('id_token');
+  const accessToken = url.searchParams.get('access_token');
 
   try {
     const resp = await fetch(
       `https://sublime-cactus-e01a5ec7f1.strapiapp.com/api/connect/google/callback?access_token=${accessToken}`
     );
-    const data = await resp.json();
-    console.log(data);
     return redirect('/');
   } catch (e) {
+    console.error(e);
     console.error('fetch getmedi');
   }
 }
